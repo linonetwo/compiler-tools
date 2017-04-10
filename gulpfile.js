26ba11a94332e7ce18b2e1c4802bfe688a8ec50b
linonetwo <linonetwo012@gmail.com>
2017-04-10 12:42:18 +0800

    chore: simplifier

--- gulpfile.js
+++ gulpfile.js
@@ -3,31 +3,25 @@ const gulp = require('gulp')
 const del = require('del')
 const Promise = require('bluebird')
 
-gulp.task('clean', function(cb) {
+gulp.task('clean', function (cb) {
   return del(['./knowledge_modules/programming-languages-and-compilers/build'])
 })
 
-gulp.task('build', ['clean'], async function() {
+gulp.task('build', ['clean'], async function () {
   const basePath = './knowledge_modules/programming-languages-and-compilers'
 
   await fs.mkdir(`${basePath}/build`)
   const topics = await fs.readdir(`${basePath}/src`)
-  
+
   const result = await Promise.reduce(topics, async (resultObject, currentDirName) => {
     const examplePath = `${basePath}/src/${currentDirName}/example.md`
-    const example = await fs.access(examplePath)
-      .then(() => fs.readFile(examplePath, 'utf8'))
-      .catch(error => '')
+    const example = await fs.readFile(examplePath, 'utf8').catch(() => '')
 
     const principlePath = `${basePath}/src/${currentDirName}/principle.md`
-    const principle = await fs.access(principlePath)
-    .then(() => fs.readFile(principlePath, 'utf8'))
-    .catch(error => '')
+    const principle = await fs.readFile(principlePath, 'utf8').catch(() => '')
 
     const tagPath = `${basePath}/src/${currentDirName}/tags.csv`
-    const tags = await fs.access(tagPath)
-    .then(() => fs.readFile(tagPath, 'utf8'))
-    .catch(error => '')
+    const tags = await fs.readFile(tagPath, 'utf8').catch(() => '')
 
     resultObject[currentDirName] = {
       title: currentDirName,
@@ -38,6 +32,6 @@ gulp.task('build', ['clean'], async function() {
 
     return resultObject
   }, {})
-  
+
   await fs.writeFile(`${basePath}/build/main.json`, JSON.stringify(result))
 })
