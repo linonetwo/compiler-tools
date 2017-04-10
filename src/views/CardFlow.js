661aafb813c311fb1da1551afb45d729e4834513
linonetwo <linonetwo012@gmail.com>
2017-04-10 17:44:23 +0800

    feat: complete graph store with GUN

--- src/views/CardFlow.js
+++ src/views/CardFlow.js
@@ -3,42 +3,52 @@ import React from 'react'
 import Gun from 'gun'
 
 import { ResultCard } from '../components/ResultCard'
+import { SearchCard } from '../components/SearchCard'
 
 import plcKnowledge from '../../knowledge_modules/programming-languages-and-compilers/build/main'
 
 const gun = Gun()
 
+window.localStorage.clear()
+
 forEach(plcKnowledge, (item) => {
   // (a)
   const { example, principle, title, tags } = item
-  const node = gun.get(title).put({ example, principle, title, tags: tags.join(',') })
+  const node = gun.get('Note').path(title).put({ example, principle, title, tags: tags.join(',') })
   // (:Note)->(a)
-  gun.get('note').set(node)
   for (const tag of tags) {
     // (:SomeTag)->(a)
-    gun.get(tag).set(node)
+    gun.get('tags').path(tag).set(node)
   }
 })
 
 export class CardFlow extends React.Component {
   state = {
+    tags: [],
     results: []
   }
 
   componentDidMount () {
-    gun.get('note').map().val(item => {
+    gun.get('Note').map().val(item => {
       this.setState(prevState => ({
         results: [...prevState.results, item]
+      }))
+    })
+
+    gun.get('tags').map().val((notes, tag) => {
+      this.setState(prevState => ({
+        tags: [...prevState.tags, tag]
       }))
     })
   }
   render () {
     return (
       <article>
+        <SearchCard tags={this.state.tags} />
         {this.state.results.map(({ title, tags, example, principle }) =>
           <ResultCard
             key={title}
-            tags={tags.split(',')}
+            tags={tags.length > 0 ? tags.split(',') : []}
             example={example}
             principle={principle}
           />
