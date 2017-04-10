08e59422669f74073a2327763605a094a15bc886
linonetwo <linonetwo012@gmail.com>
2017-04-10 14:38:26 +0800

    feat: beautifully switch between example and principle

--- src/components/ResultCard.js
+++ src/components/ResultCard.js
@@ -1,18 +1,42 @@
 import React from 'react'
-
+import Measure from 'react-measure'
 import Markdown from 'react-markdown'
+import styled from 'styled-components'
 
 import '@blueprintjs/core/dist/blueprint.css'
 import './ResultCard.css'
 
-export class ResultCard extends React.Component {
+const HoldWidthHeight = styled.div`
+  width: ${props => props.width}px;
+  height: ${props => props.height}px;
+`
 
+export class ResultCard extends React.Component {
   static defaultProps = {
-    tags: []
+    tags: [],
   }
   state = {
     showExample: true,
+    dimensions: {
+      width: -1,
+      height: -1
+    }
   }
+
+  holdWidthHeight = (elem) =>
+  <Measure
+    onMeasure={(dimensions) => {
+      if (dimensions.width <= this.state.dimensions.width || dimensions.height <= this.state.dimensions.height) {
+        return
+      }
+      this.setState({dimensions})
+    }}
+  >
+    <HoldWidthHeight height={this.state.dimensions.height} width={this.state.dimensions.width}>
+      {elem}
+    </HoldWidthHeight>
+  </Measure>
+
   render () {
     return (
       <section
@@ -20,12 +44,13 @@ export class ResultCard extends React.Component {
         className="cardLayout pt-card pt-elevation-2 pt-interactive"
       >
       <nav className="tags">
-        {this.props.tags.map(tag => <span key={tag} className="pt-tag pt-round">{tag}</span>)}
+        {[<span key="isExample" className="tag pt-tag pt-intent-primary pt-round">{this.state.showExample ? 'example' : 'principle'}</span>,
+          ...this.props.tags.map(tag => <span key={tag} className="tag pt-tag pt-round">{tag}</span>)]}
       </nav>
         {
           this.state.showExample
-          ? <Markdown className="example markdown-left upper" source={this.props.example} />
-          : <Markdown className="principle lower" source={this.props.example} />
+          ? this.holdWidthHeight(<Markdown className="example markdown-left upper" source={this.props.example} />)
+          : this.holdWidthHeight(<Markdown className="principle lower" source={this.props.principle} />)
         }
       </section>
     )
--- src/views/CardFlow.js
+++ src/views/CardFlow.js
@@ -1,18 +1,48 @@
+import { forEach } from 'lodash'
 import React from 'react'
+import Gun from 'gun'
 
 import { ResultCard } from '../components/ResultCard'
 
 import plcKnowledge from '../../knowledge_modules/programming-languages-and-compilers/build/main'
 
+const gun = Gun()
+
+forEach(plcKnowledge, (item) => {
+  // (a)
+  const { example, principle, title, tags } = item
+  const node = gun.get(title).put({ example, principle, title, tags: tags.join(',') })
+  // (:Note)->(a)
+  gun.get('note').set(node)
+  for (const tag of tags) {
+    // (:SomeTag)->(a)
+    gun.get(tag).set(node)
+  }
+})
+
 export class CardFlow extends React.Component {
+  state = {
+    results: []
+  }
+
+  componentDidMount () {
+    gun.get('note').map().val(item => {
+      this.setState(prevState => ({
+        results: [...prevState.results, item]
+      }))
+    })
+  }
   render () {
     return (
       <article>
-        <ResultCard
-          tags={plcKnowledge.tests.tags}
-          example={plcKnowledge.tests.example}
-          principle={plcKnowledge.tests.principle}
-        />
+        {this.state.results.map(({ title, tags, example, principle }) =>
+          <ResultCard
+            key={title}
+            tags={tags.split(',')}
+            example={example}
+            principle={principle}
+          />
+        )}
       </article>
     )
   }
